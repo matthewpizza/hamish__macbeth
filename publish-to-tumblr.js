8598b48c06094e903a278267d0ec19ec9e32f0fc
Matthew Spencer <hello@thisismatthew.com>
2020-01-16 09:34:43 -0500

    Upload to Tumblr

---
+++ publish-to-tumblr.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const credentials = require("./credentials.json")
+const tumblr = require("tumblr.js").createClient(credentials.tumblr)
+
+const getCaptionFromImage = (image) => {
+    const parts = image.split("-")
+    return `s${parts[2]} e${parts[3]}`
+}
+
+const image = process.argv[2]
+const caption = getCaptionFromImage(image)
+
+tumblr.photo("hamish-macbeth", {
+    caption: caption,
+    data: image,
+}, (err, resp) => {
+    if (err) {
+        return console.error(`Could not upload ${image}`, err)
+    }
+
+    console.log(`Uploaded ${image} - ${resp.id}`)
+})
