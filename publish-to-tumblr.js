363440334ce45051d1ed547706d5efeea5054f4f
Matthew Spencer <hello@thisismatthew.com>
2020-01-21 20:18:48 -0500

    This wants a buffer or base64 string

--- publish-to-tumblr.js
+++ publish-to-tumblr.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const credentials = require("./credentials.json")
+const { readFileSync } = require("fs")
 const tumblr = require("tumblr.js").createClient(credentials.tumblr)
 
 const getCaptionFromImage = (image) => {
@@ -10,10 +11,11 @@ const getCaptionFromImage = (image) => {
 
 const image = process.argv[2]
 const caption = getCaptionFromImage(image)
+const data = Buffer.from(readFileSync(image)).toString("base64")
 
 tumblr.createPhotoPost("hamishmacbeth.tumblr.com", {
     caption: caption,
-    data: image,
+    data64: data,
 }, (err, resp) => {
     if (err) {
         return console.error(`Could not upload ${image}`, err)
