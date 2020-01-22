b3038e1a3e50b2f7c0a3519ccdcc68d8692e708d
Matthew Spencer <hello@thisismatthew.com>
2020-01-21 20:49:37 -0500

    Post to twitter too

--- publish.js
+++ publish.js
@@ -3,6 +3,7 @@
 const credentials = require("./credentials.json")
 const { readFileSync } = require("fs")
 const tumblr = require("tumblr.js").createClient(credentials.tumblr)
+const twitter = new require("twit")(credentials.twitter)
 
 const getCaptionFromImage = (image) => {
     const parts = image.split("-")
@@ -13,13 +14,41 @@ const image = process.argv[2]
 const caption = getCaptionFromImage(image)
 const data = Buffer.from(readFileSync(image)).toString("base64")
 
+twitter.post("media/upload", { media_data: data }, (err, data, resp) => {
+    if (err) {
+        return console.error(`Twitter: Could not upload ${image}`, err)
+    }
+
+    const mediaId = data.media_id_string;
+
+    twitter.post("media/metadata/create", {
+        media_id: mediaId,
+        alt_text: { text: caption }
+    }, (err, data, resp) => {
+        if (err) {
+            return console.error(`Twitter: Could not upload ${image}`, err)
+        }
+
+        twitter.post("statuses/update", {
+            status: caption,
+            media_ids: [ mediaId ],
+        }, (err, data, resp) => {
+            if (err) {
+                return console.error(`Twitter: Could not upload ${image}`, err)
+            }
+
+            console.log(`Twitter: Uploaded ${image} - ${data.id}`)
+        })
+    })
+})
+
 tumblr.createPhotoPost("hamishmacbeth.tumblr.com", {
     caption: caption,
     data64: data,
 }, (err, resp) => {
     if (err) {
-        return console.error(`Could not upload ${image}`, err)
+        return console.error(`Tumblr: Could not upload ${image}`, err)
     }
 
-    console.log(`Uploaded ${image} - ${resp.id}`)
+    console.log(`Tumblr: Uploaded ${image} - ${resp.id}`)
 })
